bda6cc11222f223ba4aec53bcc0baa30a3b33426
darshgun <darshana4it@gmail.com>
2021-02-11 05:24:37 +0530

    Configure Gatsby

--- starters/gatsby-starter-darsh/gatsby-config.js
+++ starters/gatsby-starter-darsh/gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Monorepo Tutorial`,
@@ -6,9 +8,21 @@ module.exports = {
     author: `Eric Howey`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages'),
+        helpers: path.join(__dirname, 'src/helpers'),
+        components: path.join(__dirname, 'src/components'),
+        layouts: path.join(__dirname, 'src/layouts'),
+        styles: path.join(__dirname, 'src/styles'),
+      },
+    },
     {
       resolve: `gatsby-theme-darsh`,
       options: {},
     },
   ],
-}
+};
